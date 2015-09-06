b8480fcfb0dbb5b0e55c050b61b0b4e9edf1797d
MR.Y <programming@yatsukouji.com>
2015-09-07 00:32:15 +0900

    indexとshowまで実装完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "２週目復習動画", body: "ポイントは繰り返すこと、どれだけrailsを触るか", category: "スパルタキャンプ")
+
+Post.create(title: "一言メモ", body: "いかに復習の時間を確保するか！", category: "スパルタキャンプ")
